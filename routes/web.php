8859fd892c30bebaf8183a01b1eb3ac78b157bd4
Dzyfhuba <hafidz21ub@gmail.com>
2022-01-23 11:19:05 +0700

    add api resource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PresenceController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('presence', PresenceController::class);
